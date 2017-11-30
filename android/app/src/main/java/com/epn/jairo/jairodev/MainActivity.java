bd39dac2dba6a22d9be9ddadad90e9cbb5e7b291
Jairo Vera <jairov1995@yahoo.com>
2017-11-30 15:00:05 -0500

    fin practica 3

--- android/app/src/main/java/com/epn/jairo/jairodev/MainActivity.java
+++ android/app/src/main/java/com/epn/jairo/jairodev/MainActivity.java
@@ -21,5 +21,6 @@ public class MainActivity extends AppCompatActivity {
         EditText editText = (EditText) findViewById(R.id.editText);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
