9e5f950113c9e3754f2c4c2965876996baa46046
Jairo Vera <jairov1995@yahoo.com>
2017-12-14 16:01:14 -0500

    avance en clase

---
+++ proyectopractica5/app/src/main/java/Contact.java
@@ -0,0 +1,47 @@
+/**
+ * Created by Jairo on 14/12/2017.
+ */
+
+public class Contact {
+
+    int _id;
+    String _name;
+    String _phone_number;
+
+    public Contact(){}
+
+    public Contact(int id, String name, String _phone_number){
+        this._id=id;
+        this._name=name;
+        this._phone_number=_phone_number;
+    }
+
+    public Contact(String name, String _phone_number){
+        this._name=name;
+        this._phone_number=_phone_number;
+    }
+
+    public String get_name() {
+        return _name;
+    }
+
+    public void set_name(String _name) {
+        this._name = _name;
+    }
+
+    public String get_phone_number() {
+        return _phone_number;
+    }
+
+    public void set_phone_number(String _phone_number) {
+        this._phone_number = _phone_number;
+    }
+
+    public int get_id() {
+        return _id;
+    }
+
+    public void set_id(int _id) {
+        this._id = _id;
+    }
+}
