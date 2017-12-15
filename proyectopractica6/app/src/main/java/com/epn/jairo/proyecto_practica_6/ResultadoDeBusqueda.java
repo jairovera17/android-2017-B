b54b199190b73ba336b65254a1d57072e4ba299b
Jairo Vera <jairov1995@yahoo.com>
2017-12-15 15:52:38 -0500

    enlace

---
+++ proyectopractica6/app/src/main/java/com/epn/jairo/proyecto_practica_6/ResultadoDeBusqueda.java
@@ -0,0 +1,13 @@
+package com.epn.jairo.proyecto_practica_6;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ResultadoDeBusqueda extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resultado_de_busqueda);
+    }
+}
